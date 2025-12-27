70f0733e2c47a8022163564c49f9ab9a3c4388d1
20252504009-prog <20252504009@iiitvadodara.ac.in>
2025-12-27 23:33:23 +0530

    Added comments for better code readability
    Refactor and clean up code for readability and organization.

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
-
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+// Rock Paper Scissors Game in C
+// Author: Bhakkti Gautam
+// Description: A simple console-based game where the user plays against the computer.
+// Concepts Used: conditionals, random numbers, input/output
+#include<stdio.h> // For input and output functions
+#include<stdlib.h>// For rand() and srand()
+#include<time.h>// For time() function
 
 
 
@@ -41,10 +44,12 @@
         }
 int main()
 {
-        char you,computer;
+        char you,computer; // Variables to store user and computer choices
         int n,checkresult;
+         // Seed the random number generator using current time
         srand(time(NULL));
         n=rand()%100;
+        // GENERATING COMPUTER CHOICES
         if(n<33)
         {
                 computer='s';
@@ -57,11 +62,12 @@ int main()
         {
                 computer='r';
         }
+         // Display menu and take user input
         printf("\n Enter p for paper\n Enter s for scissor\nEnter r for rock");
         scanf("\n%c",&you);
          checkresult= result(you,computer);
         printf("\nYOUR INPUT=%c\t and COMPUTER INPUT=%c\n",you,computer);
-
+        // Compare user choice with computer choice to decide the result
         if(checkresult==-1)
         {
                 printf("IT'S A DRAW");
@@ -75,5 +81,5 @@ int main()
         }
 
 
-return 0;
+return 0;//END OF PROGRAM
 }
